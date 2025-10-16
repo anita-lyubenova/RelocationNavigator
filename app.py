ef4354fb7f37e517ac61422525f7d868cdb718f2
anita-lyubenova <anitaencheva@gmail.com>
2025-10-16 15:19:15 +0200

    added leisure layer & radius

--- app.py
+++ app.py
@@ -17,13 +17,12 @@ st.sidebar.markdown("## Sidebar")
 address = st.sidebar.text_input("Enter an address:", value ="Skaldevägen 60")
 POI_radius=st.sidebar.slider('Show PoIs within X m', min_value=100, max_value=3000, value=500)
 
-address="Skaldevägen 60"
-POI_radius=500
 # Main --------------------------------------------------------
 
 # If user enters an address => find latitude and longitude
 if st.sidebar.button("Go!"):
     
+
     if address:
         geolocator = Nominatim(user_agent="Navigator")
         location = geolocator.geocode(address)
@@ -36,8 +35,16 @@ if st.sidebar.button("Go!"):
            
             # Add address marker
             folium.Marker([lat, lon], popup=address, icon=folium.Icon(color='red', icon='home')).add_to(m)
-            
-    
+            folium.Circle(
+                    location=[lat, lon],
+                    radius=POI_radius,  # in meters
+                    color='blue',       # outline color
+                    fill=True,
+                    fill_color='blue',  # fill color
+                    fill_opacity=0.2,   # transparency (0 = invisible, 1 = opaque)
+                    weight=2            # outline thickness
+                    ).add_to(m)
+
             # Built environment: get POIs within 500m
             tags = {
                 'amenity':{ 'types':['cafe', 'restaurant', 'bar', 'fast_food', 'pub'],
@@ -47,7 +54,11 @@ if st.sidebar.button("Go!"):
                 'shop': {'types':['supermarket', 'convenience', 'bakery'],
                          'color':'green',
                          'icon':'shopping-cart'
-                         }
+                         },
+                'leisure': {'types': ['dog_park', 'fitness_centre','ice_rink', 'nature_reserve'],
+                              'color':"blue",
+                              'icon': "star"
+                              }
             }
             
             #subset the dictionary so that it contains only the keys and types = readable in ox.features_from_point()
@@ -60,14 +71,19 @@ if st.sidebar.button("Go!"):
             for key,item in tags.items():
                 feature_layer = folium.FeatureGroup(name=key)
                 
+                if key in pois.columns:
+                    filtered_pois = pois[pois[key].notna() & pois['name'].notna()]
+                else:
+                    print(f"Column '{key}' not found. Available columns: {list(pois.columns)}")
+                    filtered_pois = pois.copy()  # or handle differently
                # Filter POIs for this key
-                filtered_pois = pois[pois[key].notna()]
+               # filtered_pois = pois[pois[key].notna()]
                 
                 for idx, row in filtered_pois.iterrows():
                     lon_, lat_ = row.geometry.centroid.xy
                     folium.Marker(
                         location=[lat_[0], lon_[0]],
-                        popup= f"<div style='font-size:12px; font-family:Arial; white-space:nowrap;'><b>{row[key].capitalize()}</b><br>{row.get('name', 'Unnamed')}",
+                        popup= f"<div style='font-size:12px; font-family:Arial; white-space:nowrap;'><b>{row.get(key,'N/A').capitalize()}</b><br>{row.get('name', 'Unnamed')}",
                         icon = folium.Icon(color=item['color'], icon=item['icon'])
                         
                         ).add_to(feature_layer)
